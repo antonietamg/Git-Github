9beb62b3fd657c4cd8425cd184048b6c4ddeb5ea
Antonieta Martínez-Guerrero <62116253+antonietamg@users.noreply.github.com>
2025-03-10 00:24:31 -0600

    Update nombres2.R

--- nombres2.R
+++ nombres2.R
@@ -1,22 +1,22 @@
 Añade tu nombre en el número que corresponde:
 
-1.Jasiel Ruiz
-2.Eduardo
-3. Mariana
-4. Vianey Pastran
+1.
+2.
+3. 
+4.
 5.
-6.Vanessa
-7.Luis Caballero
+6.
+7.
 8.
 9.
 10.
 11. 
 12.
-13.Omar 
+13. 
 14.
 15.
 16.
 17.
 18.
-19.Ana 
+19. 
 20.
